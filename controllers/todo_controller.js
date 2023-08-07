60cf255e81722a69274edd7d077e66f9f072b6c5
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-07 05:33:50 -0700

    impl controller functions

---
+++ controllers/todo_controller.js
@@ -0,0 +1,16 @@
+const {Todo} = require("../models/todo_model")
+
+async function getAllTodo(req,res){}
+
+async function getTodoById(req,res){}
+
+async function updateTodoById(req,res){}
+
+async function deleteTodo(req,res){}
+
+module.exports = {
+    getAllTodo,
+    getTodoById,
+    updateTodoById,
+    deleteTodo,
+}
