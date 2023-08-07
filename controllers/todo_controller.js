18833d39177b6272cbdb786087cd9a61703a9783
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-07 05:43:27 -0700

    impl addNewTodo function

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -2,6 +2,29 @@ const {Todo} = require("../models/todo_model")
 
 async function getAllTodo(req,res){}
 
+async function addNewTodo(){
+    // گرفتن عنوان و توضیحات از کاربر
+    const title = req.body.title;
+    const content = req.body.content;
+
+    // اعتبار سنجی دریافتی ها
+    if(!title || !content){
+          return res.status(400).json(
+            {
+                error : "Bad Request!"
+            }
+          )  
+    }
+
+    const todo = new Todo({
+        title: title,
+        content : content
+    })
+
+    const result = await todo.save();
+    res.json(result);
+}
+
 async function getTodoById(req,res){}
 
 async function updateTodoById(req,res){}
@@ -14,3 +37,4 @@ module.exports = {
     updateTodoById,
     deleteTodo,
 }
+
